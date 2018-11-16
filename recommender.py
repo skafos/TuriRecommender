c5e92565284b3940e6e1e677e0e8e53b5fe25df0
griffinwalkerMM <griffin@metismachine.com>
2018-11-16 09:00:04 -0500

    Get recommender on Skafos

--- recommender.py
+++ recommender.py
@@ -1,9 +1,9 @@
 import turicreate as tc
 import time
 
-#from skafos import *
+from skafos import *
 
-#ska = Skafos() #initialize Skafos
+ska = Skafos() #initialize Skafos
 
 # read in the actions and items 
 actions = tc.SFrame.read_csv('https://s3.amazonaws.com/skafos.example.data/MovieLensDataset/ml-20m/ratings.csv')
@@ -11,21 +11,22 @@ items = tc.SFrame.read_csv('https://s3.amazonaws.com/skafos.example.data/MovieLe
 
 
 # split the training and validation sets up
+ska.log("Spliting up the training and validation data", labels = ['turi_recommender'])
 training_data, validation_data = tc.recommender.util.random_split_by_user(actions, 'userId', 'movieId')
 
 # build the recommender
+ska.log("Training the recommender", labels = ['turi_recommender'])
 model = tc.recommender.create(training_data, 'userId', 'movieId')
 
 # grab the results of the model
+ska.log("Obtaining the results", labels = ['turi_recommender'])
 results = model.recommend()
 
 # print the validation data
 validation_data.print_rows(num_rows=30)
 
 # evaluate the model
+ska.log("Evaluating the model on the validation data")
 model.evaluate(validation_data)
-print(model.export_coreml())
-print(model.export_coreml("./MyRecommender.mlmodel"))
-
 
 #ska.engine.save_model(model_name, model_data, tags = ["0.1.0", "latest"], access="private").result()
