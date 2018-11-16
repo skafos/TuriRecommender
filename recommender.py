8b80061e2a8391500eaac88155dd9f8de339121a
griffinwalkerMM <griffin@metismachine.com>
2018-11-16 16:53:56 -0500

    Re-pull the repo, push recommender.py

--- recommender.py
+++ recommender.py
@@ -1,6 +1,5 @@
 import turicreate as tc
 import time
-
 from skafossdk import *
 
 ska = Skafos() #initialize Skafos
@@ -9,6 +8,11 @@ ska = Skafos() #initialize Skafos
 actions = tc.SFrame.read_csv('https://s3.amazonaws.com/skafos.example.data/MovieLensDataset/ml-20m/ratings.csv')
 items = tc.SFrame.read_csv('https://s3.amazonaws.com/skafos.example.data/MovieLensDataset/ml-20m/movies.csv')
 
+# take a sample of the dataframe to have a smaller model
+actions_pd = actions.to_dataframe()
+actions_user_sample = actions_pd['userId'].drop_duplicates().sample(10000) # sample by user id
+actions_sample_df = actions_pd[actions_pd['userId'].isin(actions_user_sample)] # only grab the records with the sample users
+actions = tc.SFrame(actions_sample_df) # convert back to an SFrame
 
 # split the training and validation sets up
 ska.log("Spliting up the training and validation data", labels = ['turi_recommender'])
@@ -29,4 +33,10 @@ validation_data.print_rows(num_rows=30)
 ska.log("Evaluating the model on the validation data", labels = ['turi_recommender'])
 model.evaluate(validation_data)
 
-ska.engine.save_model(model_name, model_data, tags = ["0.1.0", "latest"], access="private").result()
+# save the model to my models
+ska.engine.save_model('RecommenderModel', model, tags = ["0.1.0", "latest"], access="private").result()
+
+
+# save the model locally
+#model.save('./models/turi_recommender.model')
+#model.export_coreml('./models/turi_recommender.mlmodel')
